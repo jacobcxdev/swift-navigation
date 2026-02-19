00df7ecad8fb0e903066f133ac20a83dfb7b1cb7
Jacob Clayden <me@jacobcx.dev>
2026-02-19 19:35:35 +0000

    Update dependency URLs from flote-works to jacobcxdev

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -35,8 +35,8 @@ let package = Package(
     .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.5.6"),
     .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.2.0"),
-    .package(url: "https://github.com/flote-works/swift-custom-dump", from: "1.3.2"),
-    .package(url: "https://github.com/flote-works/swift-perception", branch: "flote/service-app"),
+    .package(url: "https://github.com/jacobcxdev/swift-custom-dump", from: "1.3.2"),
+    .package(url: "https://github.com/jacobcxdev/swift-perception", branch: "flote/service-app"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.4.1"),
   ]
     + (android ? [
